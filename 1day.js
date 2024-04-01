5b7239510c24913ba9e33a8b999cf6f94c9f2ce5
Alishba56 <164113933+Alishba56@users.noreply.github.com>
2024-04-01 11:23:56 +0500

    Add files via upload

---
+++ 1day.js
@@ -0,0 +1,10 @@
+//             Question no #01
+//installed Node.js typescript and VS code
+//Question no #02
+// let username: string = ' alishba' //this saves the name
+// console.log (`"hello ${username} would you like to learn some typescript today?"`) //this shows the message
+//Question no #03
+var name1 = "alishba";
+console.log(name1.toUpperCase());
+console.log(name1.toLowerCase());
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
---
+++ 1day.ts
@@ -0,0 +1,19 @@
+//             Question no #01
+
+//installed Node.js typescript and VS code
+
+//Question no #02
+//Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let username: string = "alishba"; //this saves the name
+console.log(
+  `"hello ${username} would you like to learn some typescript today?"`
+); //this shows the message
+
+//Question no #03
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let name1: string = "alishba";
+console.log(name1.toUpperCase()); //this is uppercase
+console.log(name1.toLowerCase()); // this is lowercase
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase()); //this is title case
